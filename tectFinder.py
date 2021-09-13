39cdc49d7856360decd92bb8b3f094ece3ef66ff
SpaceBug <commander.n1@mail.ru>
2021-09-13 16:01:16 +1200

    Initial commit.

---
+++ tectFinder.py
@@ -0,0 +1,24 @@
+import cmath 
+import time
+from threading import Thread
+
+from obspy import read
+from obspy import Stream
+from obspy import Trace
+from obspy.core import UTCDateTime
+
+def passFilter(st):
+
+	st.resample(200)
+
+	dictOFPassFilterValues={6:[0.75,1.5],7:[1,2],15:[0.1,18],13:[8,16],14:[12,18],}
+	newTr=st[0]
+	newTr.write('newSt.mseed')
+	newSt=Stream()
+	for i in dictOFPassFilterValues:
+		newSt.append(read('newSt.mseed').filter('bandpass', freqmin=dictOFPassFilterValues[i][0], freqmax=dictOFPassFilterValues[i][1]))
+	return(newSt)
+
+def main():
+
+main()
