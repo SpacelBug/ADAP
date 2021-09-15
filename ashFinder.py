4d4c5a2a4bdb43b7a65bdbb130ccfff7ce87a74f
SpaceBug <commander.n1@mail.ru>
2021-09-15 14:13:48 +1200

    Сделал вывод результатов в файл

--- ashFinder.py
+++ ashFinder.py
@@ -7,12 +7,14 @@ import cmath
 import time
 import os
 import logging 
+from threading import Thread
 
-logger = logging.getLogger("tectFinder.py")
+
+logger = logging.getLogger("ashFinder.py")
 logger.setLevel(logging.INFO)
 
 # create the logging file handler
-fh = logging.FileHandler(f"tectFinder.log")
+fh = logging.FileHandler(f"ashFinder.log")
 
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 fh.setFormatter(formatter)
@@ -40,11 +42,14 @@ def pass_filter(st):
 '''******************************
 |Выделение тектонических событий|
 ******************************'''
-def ash_finder(name):
 
-	st=pass_filter(read(f'mseedsForTect/20210825-00-00-00({name}).msd'))
+listOfAshesActs=[] # Словарик для тектонических событий
 
-	listOfAshActs=[] # Словарик для тектонических событий
+def ash_finder(name, stationsFiRanges, heightOfVolcanos, Coefficient):
+
+	st=pass_filter(read(f'mseedsForAshes/20210825-00-00-00({name}).msd'))
+
+	
 
 	ampId=0 # id текущего значения амплитуды
 	ampOldId=-1 # id предыдущего значения амплитуды
@@ -70,44 +75,48 @@ def ash_finder(name):
 			if ((dictOfI[3]<0)and(dictOfI[1]<0)and(dictOfI[2]<0)):
 
 				Fi=cmath.log10((dictOfI[4]+dictOfI[5])/(dictOfI[1]+dictOfI[2])).real
+				logging.info(f'Fi={Fi}, K={Coefficient}, amp={amp}, I={dictOfI[0]}')
 
-				if (Fi>stationsFiRanges[name][0])and(Fi<stationsFiRanges[name][1]):
+				if (Fi>stationsFiRanges[0])and(Fi<stationsFiRanges[1]):
 
-                    H=dictOfCoefficients[name]*Fi*amp(dictOfI[0]-1)
+					H=Coefficient*Fi*abs(amp)*(dictOfI[0]-1)
 
-                    if (H>heightOfVolcanos[name]):
+					if (H>heightOfVolcanos):
 
-					    listOfAshActs.append(str(st[0].stats.starttime+ampId/st[0].stats.sampling_rate),f' | {H}')
+						listOfAshesActs.append(f'{(st[0].stats.starttime+ampId/st[0].stats.sampling_rate)} | {H}')
 
 		ampId+=1
 		ampOldId+=1
 
-	logger.info(f"Lenght listOfAshActs for {name} = {len(listOfAshActs)}")
+	logger.info(f"Lenght listOfAshActs for {name} = {len(listOfAshesActs)}")
 	logger.info(f"End {name} cycle")
 
-	return(listOfAshActs)
+	return(listOfAshesActs)
 
 def main():
 
-	listOfAshs=[]
+	listOfAshes=[]
 
 	logger.info('Prog Started')
 
 	stationsNames={"SRK":0} # Листик для записи потоков
-    stationsFiRanges={"SRK":[-1.1,1.333]} # Хранит диапозоны в которых должен лежать Fi
-    heightOfVolcanos={"SRK":3 307} # Высоты вулканов
-    dictOfCoefficients={"SRK":1.333} # Коэфициенты для расета высоты
+	stationsFiRanges={"SRK":[-1.1,1.333]} # Хранит диапозоны в которых должен лежать Fi
+	heightOfVolcanos={"SRK":3307} # Высоты вулканов
+	dictOfCoefficients={"SRK":1.333} # Коэфициенты для расета высоты
 
 	threads=[] 
 
 	for name in stationsNames:
 
-		listOfAshs=(threads.append(Thread(target=tect_finder, args=(name,stationsFiRanges,))))
-		logging.info(f'Treads list = {threads}')
+		listOfAshes.append(threads.append(Thread(target=ash_finder, args=(name,stationsFiRanges[name],heightOfVolcanos[name],dictOfCoefficients[name],))))
 		threads[stationsNames[name]].start()
 
 	for name in stationsNames:
 		threads[stationsNames[name]].join()
 
+	f=open('ashActions.txt','w+')
+	for line in listOfAshesActs:
+		f.write(f'{line}\n')
+	f.close()
 
 main()
