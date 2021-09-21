e36c673527e26259d8e622c9fba4132361541f7c
SpaceBug <commander.n1@mail.ru>
2021-09-21 12:28:45 +1200

    Минорные правки и коменты

--- weed_out.py
+++ weed_out.py
@@ -4,34 +4,40 @@
 
 from obspy.core import UTCDateTime
 
-f = open("tectActsCoins.txt", 'r')
-dictOfActs={}
-times=[]
-weedOutTimes=[]
-for line in f:
-	dictOfActs[line[0:28]]=line[31:32]
-	t=UTCDateTime(line[0:28])
-	times.append(t)
+def main():
 
-f.close()
+    f = open("tectActsCoins.txt", 'r')
 
-starttime=times[0]
-endtime=0
+    dictOfActs={} # Дублирует словарь из файла
+    times=[] # Список со всем временем
+    weedOutTimes=[] # Отсеянный список
 
-i=1
-for time in times[1:]:
-	if ((time-times[i-1])>60):
-		endtime=times[i-1]
-		weedOutTimes.append(f'{starttime}>|<{endtime}')
-		starttime=time
-	if (i==len(times)-1):
-		endtime=times[i]
-		weedOutTimes.append(f'{starttime}>|<{endtime}')
-	i+=1
+    for line in f:
+        dictOfActs[line[0:28]]=line[31:32]
+        t=UTCDateTime(line[0:28])
+        times.append(t)
 
-print('              START TIME        >|          ENDTIME')
+    f.close()
 
-i=0
-for time in weedOutTimes:
-	print('{:0>3})'.format(i), f'{time}')
-	i+=1
+    starttime=times[0]  # Время начала события
+    endtime=0  # Время конца события
+
+    i=1 # Счетчик
+    for time in times[1:]:
+        if ((time-times[i-1])>60): # Если разница более 60с., то сохроняе промежуток
+            endtime=times[i-1]
+            weedOutTimes.append(f'{starttime}>|<{endtime}')
+            starttime=time
+        if (i==len(times)-1): # Условие для учета последней записи в списке
+            endtime=times[i]
+            weedOutTimes.append(f'{starttime}>|<{endtime}')
+        i+=1
+
+    print('              START TIME        >|          ENDTIME') # Красивость
+
+    i=0 # Счетчик
+    for time in weedOutTimes:
+        print('{:0>3})'.format(i), f'{time}')
+        i+=1
+
+main()
